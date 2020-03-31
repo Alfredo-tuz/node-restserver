733d50721fc5144c951f3ac5a0e785f30302b32f
Alfredo <alfredotuz@techsystems.com.mx>
2020-03-30 20:42:19 -0600

    Rest server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,28 @@
  * 
  */
 process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * 
+ * Entorno
+ * 
+ * 
+ */
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+/**
+ * 
+ * Base de datos
+ * 
+ * 
+ */
+
+let urlDb;
+if (process.env.NODE_ENV === 'dev') {
+    urlDb = 'mongodb://localhost:27017/medsi_sativa';
+} else {
+    urlDb = 'mongodb+srv://admin:qwerty123@cluster0-aw5st.mongodb.net/prueba';
+}
+
+process.env.URLDB = urlDb;
+
+//localhost:27017/prueba
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 require('./config/config');
 
 
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+let Username = "developer";
+let UserPwd = "developer";
 
 const bodyParser = require('body-parser');
 
@@ -10,32 +14,20 @@ app.use(bodyParser.urlencoded({ extended: false })); //middlewares
 
 app.use(bodyParser.json()); //middlewares
 
-app.get('/usuario', function(req, res) {
-    res.json('Get Usuario');
+app.use(require('./routes/usuario'));
+
+mongoose.connect('mongodb+srv://admin:qwerty123@cluster0-aw5st.mongodb.net/prueba', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (err) throw err;
+
+    console.log("Base de datos online");
 });
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json(body);
-    }
-});
+mongoose.set('useCreateIndex', true);
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log("escuchando", process.env.PORT);
