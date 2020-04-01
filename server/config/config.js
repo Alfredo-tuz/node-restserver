127886e23fe2992c6a2d146cb431309936e6b3d2
Alfredo <alfredotuz@techsystems.com.mx>
2020-03-31 19:03:56 -0600

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -24,7 +24,7 @@ let urlDb;
 if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://localhost:27017/medsi_sativa';
 } else {
-    urlDb = 'mongodb+srv://admin:qwerty123@cluster0-aw5st.mongodb.net/prueba';
+    urlDb = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDb;
